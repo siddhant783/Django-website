7d84cec0276ac0f782561fb200c4b3aebce3b8b0
Siddhant Yerolkar <siddhantyerolkar@gmail.com>
2026-01-09 17:27:50 +0530

    Delete base directory

--- base/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, HttpResponse 
-
-# Create your views here.
-def home(request):
-    return render( request,'base/home.html')
-def about(request):
-    return render(request,'base/about.html')
-
